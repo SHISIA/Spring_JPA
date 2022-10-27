3a0043a5bb4d1035eaac9be4494764194b2b577c
apple <jamesshisia6@gmail.com>
2022-10-27 10:56:13 +0300

    Setting up the application.properties file configurations and Creating a Student entity

---
+++ src/main/java/com/_jpa/Spring_JPA/entity/Student.java
@@ -0,0 +1,44 @@
+package com._jpa.Spring_JPA.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="table_student",
+uniqueConstraints = @UniqueConstraint(
+        name = "emailId_unique",
+        columnNames = "email_address"
+))
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence")
+    @Column(
+            name = "id",
+            nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
+    private String emailId;
+    private String guardianName;
+    private String getGuardianEmail;
+    private String getGuardianMobile;
+}
